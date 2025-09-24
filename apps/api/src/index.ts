f26b6a798c501188f2f0f90451c247207268451d
Edyane <edyanealves@gmail.com>
2025-09-24 10:15:51 -0300

    feat: verificando as permissões

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,12 @@
-import { ability } from '@repo/auth'
+import { defineAbilityFor } from "@repo/auth";
 
-const userCanInviteSomeoneElese = ability.can('invite', 'User')
-const userCanDeleteOtherUsers = ability.can('delete', 'User')
+const ability = defineAbilityFor({ role: "MEMBER" });
 
-const userCannotDeleteOtherUsers = ability.cannot('delete', 'User')
+const userCanInviteSomeoneElese = ability.can("invite", "User");
+const userCanDeleteOtherUsers = ability.can("delete", "User");
 
-console.log(userCanInviteSomeoneElese)
-console.log(userCanDeleteOtherUsers)
-console.log(userCannotDeleteOtherUsers)
+const userCannotDeleteOtherUsers = ability.cannot("delete", "User");
+
+console.log(userCanInviteSomeoneElese);
+console.log(userCanDeleteOtherUsers);
+console.log(userCannotDeleteOtherUsers);
