6981bd32d862d7621e829303c8500ef2c04b681d
Edyane <edyanealves@gmail.com>
2025-10-01 08:24:53 -0300

    refactor: adicionando zod e importando roles ao arquivo de permissões

--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -1,21 +1,22 @@
-import { AbilityBuilder } from "@casl/ability";
+import { AbilityBuilder } from '@casl/ability'
 
-import { AppAbility } from ".";
-import { User } from "./models/user";
-
-type Role = "ADMIN" | "MEMBER";
+import { AppAbility } from '.'
+import { User } from './models/user'
+import { Role } from './roles'
 
 type PermissionsByRole = (
   user: User,
   builder: AbilityBuilder<AppAbility>,
-) => void;
+) => void
 
 export const permissions: Record<Role, PermissionsByRole> = {
   ADMIN(_, { can }) {
-    can("manage", "all");
+    can('manage', 'all')
   },
   MEMBER(_, { can }) {
-    can("invite", "User");
-    can("manage", "Project");
+    // can('invite', 'User')
+    can('manage', 'Project')
   },
-};
+  BILLING() {},
+}
+
