1dabe38f996f336a24fa586e0e4f8a81dba46afe
Edyane <edyanealves@gmail.com>
2025-10-02 16:07:21 -0300

    feat: ajustando permissão para que apenas donos ajustem seus projetos

--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -1,22 +1,22 @@
-import { AbilityBuilder } from '@casl/ability'
+import { AbilityBuilder } from "@casl/ability";
 
-import { AppAbility } from '.'
-import { User } from './models/user'
-import { Role } from './roles'
+import { AppAbility } from ".";
+import { User } from "./models/user";
+import { Role } from "./roles";
 
 type PermissionsByRole = (
   user: User,
   builder: AbilityBuilder<AppAbility>,
-) => void
+) => void;
 
 export const permissions: Record<Role, PermissionsByRole> = {
   ADMIN(_, { can }) {
-    can('manage', 'all')
+    can("manage", "all");
   },
-  MEMBER(_, { can }) {
+  MEMBER(user, { can }) {
     // can('invite', 'User')
-    can('manage', 'Project')
+    can(["create", "get"], "Project");
+    can(["update", "delete"], "Project", { ownerId: { $eq: user.id } });
   },
   BILLING() {},
-}
-
+};
